b28c4d234529fa6e0ca253b42f075a47edd9961c
bertolinocastro <bertolinocastro@gmail.com>
2023-03-25 23:51:20 -0300

    começando do zero pq essa caralha do qt creator é bugada

---
+++ includes/windowprincipal.h
@@ -0,0 +1,24 @@
+#ifndef WINDOWPRINCIPAL_H
+#define WINDOWPRINCIPAL_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class WindowPrincipal; }
+QT_END_NAMESPACE
+
+class WindowPrincipal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    WindowPrincipal(QWidget *parent = nullptr);
+    ~WindowPrincipal();
+
+private:
+    Ui::WindowPrincipal *ui;
+
+private slots:
+    void novoJogo();
+};
+#endif // WINDOWPRINCIPAL_H
