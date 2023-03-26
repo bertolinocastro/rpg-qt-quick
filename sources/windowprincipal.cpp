cd9c9b9f69c3ecda7b3fa0e6e0bd5c51ccfb47db
bertolinocastro <bertolinocastro@gmail.com>
2023-03-26 00:21:24 -0300

    Consegui fazer o connect e funcionar o signal x slot

--- sources/windowprincipal.cpp
+++ sources/windowprincipal.cpp
@@ -1,11 +1,15 @@
 #include "windowprincipal.h"
 #include "ui_windowprincipal.h"
+#include <iostream>
 
 WindowPrincipal::WindowPrincipal(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::WindowPrincipal)
 {
     ui->setupUi(this);
+//    QPushButton *btnNovoJogo = ;
+    QObject::connect(ui->pushButtonNovoJogo, &QPushButton::clicked, this, &WindowPrincipal::novoJogo);
+
 }
 
 WindowPrincipal::~WindowPrincipal()
@@ -14,6 +18,7 @@ WindowPrincipal::~WindowPrincipal()
 }
 
 void WindowPrincipal::novoJogo(){
-
+    std::cout << "Entrou no slot Novo Jogo" <<std::endl;
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
