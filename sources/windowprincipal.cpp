71f1c06e78a867c7bf89f5231b8a3989b0ba034d
bertolinocastro <bertolinocastro@gmail.com>
2023-03-26 15:54:29 -0300

    graphics view entrando por slot X signal e leitura de mapa sendo iniciada

--- sources/windowprincipal.cpp
+++ sources/windowprincipal.cpp
@@ -1,12 +1,14 @@
 #include "windowprincipal.h"
 #include "ui_windowprincipal.h"
 #include <iostream>
+#include <map.h>
 
 WindowPrincipal::WindowPrincipal(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::WindowPrincipal)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(1);
 //    QPushButton *btnNovoJogo = ;
     QObject::connect(ui->pushButtonNovoJogo, &QPushButton::clicked, this, &WindowPrincipal::novoJogo);
 
@@ -20,5 +22,12 @@ WindowPrincipal::~WindowPrincipal()
 void WindowPrincipal::novoJogo(){
     std::cout << "Entrou no slot Novo Jogo" <<std::endl;
     ui->stackedWidget->setCurrentIndex(0);
+
+    iniciaGraphicsView("inicial.txt");
 }
 
+void WindowPrincipal::iniciaGraphicsView(std::string fase){
+
+    Map mapa = Map(fase);
+
+}
